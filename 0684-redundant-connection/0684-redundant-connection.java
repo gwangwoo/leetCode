776854fc34aa2125901cb2a19d97a129b10da872
gguu <rhkddn5161@naver.com>
2026-04-21 08:34:56 +0900

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -1,16 +1,17 @@
 class Solution {
-    static int[] parent;
+    static int[] p;
     static int N;
     public static int[] findRedundantConnection(int[][] edges) {
         N = edges.length;
-        parent = new int[N+1];
+        p = new int[N+1];
         for (int i = 1 ; i <= N ; i++) {
-            parent[i] = i;
+            p[i] = i;
         }
 
         for (int[] edge : edges) {
             int u = edge[0];
             int v = edge[1];
+
             if (find(u) == find(v)) {
                 return edge;
             }
@@ -18,16 +19,17 @@ class Solution {
         }
         return new int[0];
     }
+
     private static void union(int u, int v) {
         u = find(u);
         v = find(v);
         if (u != v) {
-            parent[u] = v;
+            p[u] = v;
         }
     }
 
-    private static int find(int v) {
-        if (parent[v] == v) return v;
-        return parent[v] = find(parent[v]);
+    private static int find(int u) {
+        if (p[u] == u) return u;
+        return p[u] = find(p[u]);
     }
 }
